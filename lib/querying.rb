a34b280d34e8d9f19ad280acc92eaaa57fb1588b
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 20:12:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
   ON authors.id = characters.author_id
   WHERE species = 'human'
   ORDER BY count(species) DESC
-  LIMIT 1;"
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
