554fac62d4802833461460b114b2df79bed9451b
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 18:59:35 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,25 +2,25 @@ CREATE TABLE series (
   title TEXT PRIMARY KEY,
   author_id INTEGER,
   subgenre_id INTEGER
-)
+);
 
 CREATE TABLE subgenres (
   name TEXT PRIMARY KEY
-)
+);
 
 CREATE TABLE authors (
   name TEXT PRIMARY KEY
-)
+);
 
 CREATE TABLE books (
   title TEXT PRIMARY KEY,
   year INTEGER,
   series_id INTEGER
-)
+);
 
 CREATE TABLE characters (
   name TEXT PRIMARY KEY,
   species TEXT,
   motto TEXT,
   author_id INTEGER
-)
+);
