46d48c3bb4caf4f2ee21278d3e912ddf439f2e95
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 19:37:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON authors.id = series.authors_id
+  ON authors.id = series.author_id
   JOIN subgenres
   ON subgenre.id = series.subgenre.id;"
 end
